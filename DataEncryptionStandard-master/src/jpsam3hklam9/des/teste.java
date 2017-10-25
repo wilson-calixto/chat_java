c096a0997aa1c64a136cb7ea7933ca4daa35a9b2
wilsoncalisto <wccj.snf@uea.edu.br>
2017-10-25 18:24:41 -0400

    des adicionado

---
+++ DataEncryptionStandard-master/src/jpsam3hklam9/des/teste.java
@@ -0,0 +1,11 @@
+
+/**
+ * Class to provide a simple user interface to the DES algorithm.
+ */
+public class teste
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Input plaintext: \n%s", new String(text));
+    }
+}
